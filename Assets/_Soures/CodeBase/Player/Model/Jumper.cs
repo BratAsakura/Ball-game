7778058fe990a8dbbcb7711f2264345eb2538064
BratAsakura <brat09141@gmail.com>
2026-05-18 19:26:50 +0300

    [ADD] - double jump
    Добавил двойной прыжок + пофиксил бесконечный прыжок

--- Assets/_Soures/CodeBase/Player/Model/Jumper.cs
+++ Assets/_Soures/CodeBase/Player/Model/Jumper.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Jumper : MonoBehaviour, ICanJump
 {
+    private const int DefaultJumpingCount = 1;
+
     [SerializeField] private float _jumpForce = 3;
+    [SerializeField] private int _jumpCount = DefaultJumpingCount;
+    [SerializeField] private Transform _feets;
+    [SerializeField] private LayerMask _groundLayer;
 
     private Rigidbody2D _rigidbody;
 
@@ -12,13 +17,24 @@ public class Jumper : MonoBehaviour, ICanJump
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (IsGrounded() == true)
+            _jumpCount = DefaultJumpingCount;
+    }
+
     public bool IsGrounded()
     {
-        throw new System.NotImplementedException();
+        RaycastHit2D hit = Physics2D.Raycast(_feets.position, Vector2.down, 0.1f, _groundLayer);
+        return hit.collider != null;
     }
 
     public void Jump()
     {
+        if (IsGrounded() == false && _jumpCount == 0)
+            return;
+
         _rigidbody.AddForceY(_jumpForce, ForceMode2D.Impulse);
+        _jumpCount--;
     }
 }

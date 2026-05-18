f7ad7b258a449a87fc5595de14104b6522f5878f
BratAsakura <brat09141@gmail.com>
2026-05-18 20:01:23 +0300

    [Fix] - switched to rigitbody2d

--- Assets/_Soures/CodeBase/Player/Model/Movement.cs
+++ Assets/_Soures/CodeBase/Player/Model/Movement.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour, IMovable
 {
     [SerializeField] private float _speed = 4;
 
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
     public void Move(float direction)
     {
-        transform.Translate(new Vector2(direction * _speed * Time.deltaTime, 0));
+        _rigidbody.linearVelocity = new Vector2(direction * _speed, _rigidbody.linearVelocity.y);
     }
 
     public void StopMove()
     {
-        transform.Translate(new Vector2(0, 0));
+        _rigidbody.linearVelocity = new Vector2(0f * _speed, _rigidbody.linearVelocity.y);
     }
 }
